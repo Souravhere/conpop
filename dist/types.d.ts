4d61cf668ac113fd4ce75a3461a315d532db8571
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-07 19:46:45 +0530

    build run to create the dist

---
+++ dist/types.d.ts
@@ -0,0 +1,11 @@
+export interface PrintOptions {
+    color?: string;
+    background?: string;
+    size?: string;
+    bold?: boolean;
+    italic?: boolean;
+}
+export interface PrintData {
+    content: string | string[];
+    options?: PrintOptions;
+}
